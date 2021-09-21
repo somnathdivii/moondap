fac4cf6081bb9f7041bd14c3d3affab085758ed1
Somnath <61135875+somnathdivii@users.noreply.github.com>
2021-09-21 12:37:20 +0530

    update pdf renderting

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -10,6 +11,38 @@ app.use(express.static(__dirname + '/public'));
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
 }
+
+
+app.get('/showpdf', function (req, res) {
+
+  
+  var filePath = "/public/sample.pdf";
+
+  fs.readFile(__dirname + filePath , function (err,data){
+      res.contentType("application/pdf");
+      res.send(data);
+  });
+
+});
+
+
+// app.post('/url/pdf', function(req, res, next) {
+//   var stream = fs.readStream('/location/of/pdf');
+//   var filename = "WhateverFilenameYouWant.pdf"; 
+//   // Be careful of special characters
+
+//   filename = encodeURIComponent(filename);
+//   // Ideally this should strip them
+
+//   res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+//   res.setHeader('Content-type', 'application/pdf');
+
+//   stream.pipe(res);
+// });
+
+
+
+
 
 io.on('connection', onConnection);
 
