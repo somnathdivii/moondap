e500f809de27569d31437c00d87aa1dd52c1d32b
banerjeesubhajit <71057796+banerjeesubhajit@users.noreply.github.com>
2021-09-22 19:09:30 +0530

    update

--- index.js
+++ index.js
@@ -42,53 +42,33 @@ io.on('connection', function (socket) {
   var filePath = "/sample.pdf";
   socket.emit("sendfile", { filePath, clickCount });
 
-  // fs.readFile(__dirname + filePath, function (error, filedata) {
-  //   if (error) throw error;
-  //   else socket.emit("sendfile",  filePath);
-  // });
-
-  //when the server receives clicked message, do this
-
-
-
-
-
-
-
 
   socket.on('nextclicked', function (data) {
+
+    var page_cnt = data;
+    if(clickCount >= page_cnt)
+    {
+       return false;
+    }
     clickCount++;
 
-    io.emit('buttonUpdate', clickCount);
+    var filePath = "/sample.pdf";
+    io.emit('buttonUpdate', {filePath,clickCount});
   });
 
 
   socket.on('previousclicked', function (data) {
+
+    if (clickCount <= 1) {
+      return;
+    }
     clickCount--;
 
-    io.emit('buttonUpdate', clickCount);
+    io.emit('buttonUpdate', {filePath,clickCount});
   });
 
   
 });
-
-
-// app.post('/url/pdf', function(req, res, next) {
-//   var stream = fs.readStream('/location/of/pdf');
-//   var filename = "WhateverFilenameYouWant.pdf"; 
-//   // Be careful of special characters
-
-//   filename = encodeURIComponent(filename);
-//   // Ideally this should strip them
-
-//   res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
-//   res.setHeader('Content-type', 'application/pdf');
-
-//   stream.pipe(res);
-// });
-
-
-
 
 
 // io.on('connection', onConnection);
